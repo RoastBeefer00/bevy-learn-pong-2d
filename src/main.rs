1d830e3e518f3d49441179d9f75c21e14ad0b9dc
Jake Jasmin <roastbeefer000@gmail.com>
2024-10-01 16:32:15 -0600

    pong sort of

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use bevy::prelude::*;
+use bevy::{ecs::component, prelude::*};
+use rand::Rng;
 
 fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins);
-    app.add_systems(Startup, (spawn_camera, spawn_paddles));
-    app.add_systems(Update, move_paddle);
+    app.add_systems(Startup, (spawn_camera, spawn_paddles, spawn_ball));
+    app.add_systems(Update, (move_paddle, move_ball, ball_collide));
     app.run();
 }
 
@@ -61,11 +62,63 @@ fn move_paddle(
     time: Res<Time>
 ) {
     for (mut pos, settings) in &mut paddles {
-        if input.pressed(settings.move_up) && pos.translation.y < 170.0 {
-            pos.translation.y += 100.0 * time.delta_seconds();
+        if input.pressed(settings.move_up) {
+            pos.translation.y += 200.0 * time.delta_seconds();
+            pos.translation.y = pos.translation.y.clamp(-170.0, 170.0);
         }
-        if input.pressed(settings.move_down) && pos.translation.y > -170.0 {
-            pos.translation.y -= 100.0 * time.delta_seconds();
+        if input.pressed(settings.move_down) {
+            pos.translation.y -= 200.0 * time.delta_seconds();
+            pos.translation.y = pos.translation.y.clamp(-170.0, 170.0);
+        }
+    }
+}
+
+#[derive(Component)]
+struct Ball(Vec2);
+
+fn spawn_ball(mut commands: Commands) {
+    commands.spawn((SpriteBundle {
+        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 1.0)),
+        sprite: Sprite {
+            color: Color::WHITE,
+            custom_size: Some(Vec2::new(25.0, 25.0)),
+            ..default()
+        },
+        ..default()
+    }, Ball(Vec2::new(-100.0, 0.0))));
+}
+
+fn move_ball(
+    mut balls: Query<(&mut Transform, &Ball)>,
+    time: Res<Time>,
+) {
+    for (mut pos, ball) in &mut balls {
+        pos.translation += ball.0.extend(0.0) * time.delta_seconds();
+    }
+}
+
+const BWIDTH: f32 = 25.0;
+const PWIDTH: f32 = 25.0;
+const PHEIGHT: f32 = 25.0;
+
+
+fn ball_collide(
+    mut balls: Query<(&Transform, &mut Ball)>,
+    paddles: Query<&Transform, With<Paddle>>
+) {
+    for (ball, mut velocity) in &mut balls {
+        if ball.translation.y.abs() + BWIDTH / 2.0 > 250.0 {
+            velocity.0.y *= -1.0;
+        }
+        for paddle in &paddles {
+            if 
+                ball.translation.x - BWIDTH / 2.0 < paddle.translation.x + PWIDTH / 2.0 &&
+                ball.translation.y - BWIDTH / 2.0 < paddle.translation.y + PHEIGHT / 2.0 &&
+                ball.translation.x + BWIDTH / 2.0 > paddle.translation.x - PWIDTH / 2.0 &&
+                ball.translation.y + BWIDTH / 2.0 > paddle.translation.y - PHEIGHT / 2.0 {
+                    velocity.0 *= -1.0;
+                    velocity.0.y = rand::thread_rng().gen::<f32>() * 100.0;
+            }
         }
     }
 }
